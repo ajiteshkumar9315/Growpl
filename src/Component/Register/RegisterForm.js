80c6f61e8818b543625d05698f469f79c9a9320d
ajiteshkumar9315 <ajiteshkumar9315888@gmail.com>
2023-10-23 21:47:10 +0530

    adding login css and do some cleaning

--- src/Component/Register/RegisterForm.js
+++ src/Component/Register/RegisterForm.js
@@ -68,21 +68,21 @@ const RegisterForm = () => {
     <div className='contan '>
       <form className="row " method='POST'>
   <div className="col-md-5 m-3 mx-4">
-    <label htmlFor="validationDefault01" className="form-label p-3">First name</label>
+    <label htmlFor="validationDefault01" className="form-label fw-bolder p-3">First name</label>
     <input type="text" className="form-control" id="validationDefault01" name="firstName" value={user.firstName} onChange={getUserData} autoComplete='off' placeholder='Enter The FirstName' required/>
   </div>
   <div className="col-md-5 m-3">
-    <label htmlFor="validationDefault02" className="form-label p-3">Last name</label>
+    <label htmlFor="validationDefault02" className="form-label fw-bolder p-3">Last name</label>
     <input type="text" className="form-control" id="validationDefault02" name="lastName" value={user.lastName} onChange={getUserData} autoComplete='off' placeholder='Enter The LastName' required/>
   </div>
   <div className="col-md-5 m-3 mx-4">
-    <label htmlFor="validationDefaultUsername" className="form-label p-3">Email</label>
+    <label htmlFor="validationDefaultUsername" className="form-label fw-bolder p-3">Email</label>
     <div className="input-group">
       <input type="email" className="form-control" id="validationDefaultUsername" aria-describedby="inputGroupPrepend2" name='email' value={user.email} onChange={getUserData} autoComplete='off' placeholder='Enter The Email' required/>
     </div>
   </div>
   <div className="col-md-5 m-3 mx-4">
-    <label htmlFor="validationDefault03" className="form-label p-3">City</label>
+    <label htmlFor="validationDefault03" className="form-label fw-bolder p-3">City</label>
     <input type="text" className="form-control" id="validationDefault03" name='city' value={user.city} onChange={getUserData} autoComplete='off' placeholder='Enter The city' required/>
   </div>
   {/* <div className="col-md-3">
@@ -93,12 +93,12 @@ const RegisterForm = () => {
     </select>
   </div> */}
   <div className="col-md-5 m-3 mx-4">
-    <label htmlFor="validationDefault05" className="form-label p-3">Mobile Number</label>
+    <label htmlFor="validationDefault05" className="form-label fw-bolder p-3">Mobile Number</label>
     <input type="text" className="form-control" id="validationDefault05" name='Phone' value={user.Phone} onChange={getUserData} autoComplete='off' placeholder='Enter The Mobile Number' required/>
   </div>
  
   <div className="col-12 m-5">
-    <button className="btn btn-dark w-25 h-100 rounded-pill" type="submit" onClick={postData}>Submit </button>
+    <button className="btn btn-dark fw-bolder w-25 h-100 rounded-pill " type="submit" onClick={postData}>Submit </button>
   </div>
 </form>
     </div>
